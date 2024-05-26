1aa57375bdf3f775e9b59c5eb5e35a84ce0d56d1
Camille Gontier <cam.gontier@gmail.com>
2024-05-25 22:11:58 -0400

    Basic parameters values
    No recurrent connections, and otherwise same parameter values as in Fig 4d in https://link.springer.com/article/10.1007/s00422-008-0264-7

--- Fig3B.py
+++ Fig3B.py
@@ -12,93 +12,77 @@ from brian2 import *
 
 # Parameters ##################################################################
 
-duration = 4*second
-
+duration = 20*second
 # Neuronal population
-N_E = 100
-
-# Strength
-we = (0.67*2)*mV # excitatory synaptic weight (voltage)
-
-# Membrane time constants
-tau_E = 0.01*second
-tau_A = 0.15*second
-
-g_A = 10*mV
-
-threshold = 20*mV
-reset = 0*volt
-
+N_E = 1000
+# Total capacitance
+C = 200*pF 
+# Total leak capacitance
+gL = 10*nS
+# Effective rest potential
+EL = -58*mV
+# Threshold slope factor
+DeltaT = 2*mV
+# Effective threshold potential
+VT = -50*mV
+# Conductance
+a = 2*nS
+# Time constant
+tauw = 120*msecond
+# Adaptation
+b = 100*pA
+# Reset potential
+Vr = -46*mV
+# Noise
+# sigma = 1*mV
 # External input
-I_E_values = 21*mV
+I_values = 210*pA
 
-sigma = 1*mV
-
-plot_offset = 70
+# Synaptic parameters
+connect_prob = 0.00
+we = 0*mV
 
 # Equations ###################################################################
 
 eqs_E = Equations('''
-dV_E/dt = (I_E - V_E - A_E)/tau_E + sigma*sqrt(2/tau_E)*xi : volt
-dA_E/dt = - A_E/tau_A : volt
-I_E : volt
+dV/dt = (-gL*(V-EL) + gL*DeltaT*exp((V-VT)/DeltaT) + I - w)/C : volt
+dw/dt = (a*(V-EL) - w)/tauw : amp
+I : amp
 ''')
 
 G_E = NeuronGroup(N_E, 
                 model=eqs_E,
-                threshold='V_E > threshold', 
-                reset='V_E=reset; A_E+=g_A',
-                refractory=3*ms)
-G_E.I_E = I_E_values
+                threshold='V > 0*mV', 
+                reset='V=Vr; w+=b',
+                )
 
-S_EE = Synapses(G_E, G_E, on_pre='V_E += we')
+G_E.I = I_values
 
-C = np.ones(N_E,dtype=int) - np.identity(N_E,dtype=int)  # The connection matrix as a numpy array of 0's and 1's
-sources, targets = C.nonzero()
-# S_EE.connect(i=sources, j=targets)
-S_EE.connect(p=0.5)
+S_EE = Synapses(G_E, G_E, on_pre='V += we')
+
+S_EE.connect(p=connect_prob)
 
 spikemon = SpikeMonitor(G_E)
-M = StateMonitor(G_E, 'V_E', record=True)
-P = StateMonitor(G_E, 'A_E', record=True)
+M = StateMonitor(G_E, 'V', record=True)
+P = StateMonitor(G_E, 'w', record=True)
 
-G_E.V_E = 'rand() * 20*mV '
+G_E.V = 'EL+EL*rand() * 0.1 '
 
 run(duration)
 
 # Results #####################################################################
 
 fig,axs=plt.subplots(nrows=2,ncols=1,sharex=True)
-axs[0].plot(M.t/ms,M.V_E[0]/mV-plot_offset)
-axs[0].axhline(y=threshold/mV-plot_offset,linestyle='--',c='k')
-axs[1].axhline(y=threshold/mV-plot_offset,linestyle='--',c='k')
+axs[0].plot(M.t/ms,M.V[0]/mV)
+axs[0].axhline(y=VT/mV,linestyle='--',c='k')
+axs[1].axhline(y=VT/mV,linestyle='--',c='k')
 
-axs[1].plot(M.t/ms,M.V_E[1]/mV-plot_offset)
+axs[1].plot(M.t/ms,M.V[1]/mV)
 axs[1].set_xlabel('Time [ms]')
 axs[0].set_ylabel('V [mV]')
 axs[1].set_ylabel('V [mV]')
 axs[0].grid()
 axs[1].grid()
 fig.tight_layout()
-savefig("Fig3B.png", dpi=300) 
+savefig("Fig3B.png", dpi=200) 
 
-# plt.figure()
-# plot(spikemon.t/ms, spikemon.i, '.k')
-# xlabel('Time (ms)')
-# ylabel('Neuron index')
-
-# plt.figure()
-# plot(M.t/ms, M.V_E[1]/mV)
-# plot(M.t/ms, M.V_E[2]/mV)
-# plot(M.t/ms, M.V_E[3]/mV)
-# plot(M.t/ms, M.V_E[4]/mV)
-# plot(M.t/ms, M.V_E[5]/mV,linestyle='--')
-# xlabel('t (ms)')
-# ylabel('v (mV)')
-# show() 
-
-# plt.figure()
-# plot(M.t/ms, P.A_E[1]/mV)
-# xlabel('t (ms)')
-# ylabel('v (mV)')
-# show() 
